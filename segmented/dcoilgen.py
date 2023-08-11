0f5cf964bd0eb8089f8e0eaedcd4ab2ccba2510f
Sakib Rahman <rahmans@myumanitoba.ca>
2023-08-11 11:06:44 -0400

    Fix syntax errors related to improper concatenation of strings

--- segmented/dcoilgen.py
+++ segmented/dcoilgen.py
@@ -199,8 +199,8 @@ for j in range(1,4):
     out+="\n\t\t\t<rotation name=\"rotation_node_solid_"+i+str(j)+"_back\" x=\"-pi\" />"
     out+="\n\t\t</union>\n"
        
-  out+="\n\t<box lunit=\"mm\" name=\"straight_lower_solid_"+str(j)+"\" x=\"+str(p["C"+str(j)+"_dx"])+\" y=\"+str(p["C"+str(j)+"_dy"])+\" z=\""+str(p["C"+str(j)+"_z2_up"]-p["C"+str(j)+"_z1_up"])+"\"/>\n"
-  out+="\n\t<tube lunit=\"mm\" aunit=\"deg\" name=\"watertube_lower_solid_"+str(j)+"\" deltaphi=\"360\" startphi=\"0\" z=\""+str(p["C"+str(j)+"_z2_up"]-p["C"+str(j)+"_z1_up"])+"\" rmin=\"0\" rmax=\"p["C"+str(j)+"_watertube_radius"]\"/> \n \
+  out+="\n\t<box lunit=\"mm\" name=\"straight_lower_solid_"+str(j)+"\" x=\""+str(p["C"+str(j)+"_dx"])+"\" y=\""+str(p["C"+str(j)+"_dy"])+"\" z=\""+str(p["C"+str(j)+"_z2_up"]-p["C"+str(j)+"_z1_up"])+"\"/>\n"
+  out+="\n\t<tube lunit=\"mm\" aunit=\"deg\" name=\"watertube_lower_solid_"+str(j)+"\" deltaphi=\"360\" startphi=\"0\" z=\""+str(p["C"+str(j)+"_z2_up"]-p["C"+str(j)+"_z1_up"])+"\" rmin=\"0\" rmax=\""+p["C"+str(j)+"_watertube_radius"]+"\"/>\n"
 
 for j in ["mid"]:
   xoff={}
